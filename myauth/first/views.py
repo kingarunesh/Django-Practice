baf6cd805a8ba54ae728c944a1688a51d6b45c4f
Arunesh <aruneshkumar2026@gmail.com>
2024-08-02 12:53:09 +0530

    18.3 : myauth - edit default register form

--- myauth/first/views.py
+++ myauth/first/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from first.forms import RegisterForm
 
 
 
 def register(request):
     
+    #NOTE :         we can do also like this
+    # if request.method == "POST":
+    #     form = UserCreationForm(request.POST)
+        
+    #     if form.is_valid():
+    #         form.save()
+            
+    # else:
+    #     form = UserCreationForm()
+    
+    #NOTE :         custom form edit
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         
         if form.is_valid():
             form.save()
             
+            messages.success(request, "Account Register successfully...")
+            
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     
     context = {
         "form": form
