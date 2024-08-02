7e63d451f86f7441b52cd53f36370206ad7fce99
Arunesh <aruneshkumar2026@gmail.com>
2024-08-02 23:03:06 +0530

    18.8 : myauth - Blog model added

--- myauth/first/admin.py
+++ myauth/first/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from first.models import Aohan
+from first.models import Aohan, Blog
 
 
 @admin.register(Aohan)
 class AohanAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "city"]
+
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+    list_display_links = ["id", "title"]
--- myauth/first/models.py
+++ myauth/first/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Aohan(models.Model):
     name = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
