047c13cb1b9d844f606b6fa15bfe0d0d070c5be3
Arunesh <aruneshkumar2026@gmail.com>
2024-08-02 17:33:11 +0530

    18.5 : myauth - change password

--- myauth/first/urls.py
+++ myauth/first/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from first.views import register, login_view, profile_view, logout_view
+from first.views import register, login_view, profile_view, logout_view, change_password, change_password_without_old_pass
 
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path("login/", view=login_view, name="login_view"),
     path("profile/", view=profile_view, name="profile_view"),
     path("logout/", view=logout_view, name="logout_view"),
+    path("change-password/", view=change_password, name="change_password"),
+    path("change-password-1/", view=change_password_without_old_pass, name="change_password_without_old_pass"),
 ]
--- myauth/first/views.py
+++ myauth/first/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm, SetPasswordForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
 
 from first.forms import RegisterForm
@@ -90,3 +90,61 @@ def logout_view(request):
     logout(request=request)
     
     return redirect("login_view")
+
+
+def change_password(request):
+    
+    if not request.user.is_authenticated:
+        return redirect("login_view")
+    
+    if request.method == "POST":
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            messages.success(request, "Password updated")
+            # return redirect("login_view")
+            
+            #!      it will not logout and update current session with new password
+            update_session_auth_hash(request=request, user=form.user)
+            return redirect("profile_view")
+            
+        
+    else:
+        form = PasswordChangeForm(user=request.user)
+    
+    context = {
+        "form": form
+    }
+    
+    return render(request=request, template_name="first/change-password.html", context=context)
+
+
+def change_password_without_old_pass(request):
+    
+    if not request.user.is_authenticated:
+        return redirect("login_view")
+    
+    if request.method == "POST":
+        form = SetPasswordForm(user=request.user, data=request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            messages.success(request, "Password updated")
+            # return redirect("login_view")
+            
+            #!      it will not logout and update current session with new password
+            update_session_auth_hash(request=request, user=form.user)
+            return redirect("profile_view")
+            
+        
+    else:
+        form = SetPasswordForm(user=request.user)
+    
+    context = {
+        "form": form
+    }
+    
+    return render(request=request, template_name="first/pass-change-without-old-pass.html", context=context)
