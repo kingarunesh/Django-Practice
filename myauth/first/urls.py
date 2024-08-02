c065f97dc936a3273eb7b96009e1fca763657aa1
Arunesh <aruneshkumar2026@gmail.com>
2024-08-02 17:10:49 +0530

    18.4 : myauth - login | logout | profile

--- myauth/first/urls.py
+++ myauth/first/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from first.views import register
+from first.views import register, login_view, profile_view, logout_view
 
 
 urlpatterns = [
-    path("register/", view=register)
+    path("register/", view=register, name="register"),
+    path("login/", view=login_view, name="login_view"),
+    path("profile/", view=profile_view, name="profile_view"),
+    path("logout/", view=logout_view, name="logout_view"),
 ]
--- myauth/first/views.py
+++ myauth/first/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 from first.forms import RegisterForm
@@ -7,6 +8,9 @@ from first.forms import RegisterForm
 
 
 def register(request):
+    
+    if request.user.is_authenticated:
+        return redirect("profile_view")
     
     #NOTE :         we can do also like this
     # if request.method == "POST":
@@ -35,3 +39,54 @@ def register(request):
     }
     
     return render(request=request, template_name="first/register.html", context=context)
+
+
+def login_view(request):
+    
+    if request.user.is_authenticated:
+        return redirect("profile_view")
+    
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request=request, user=user)
+                
+                messages.success(request, "Login success...")
+                
+                return redirect("profile_view")
+    else:
+        form = AuthenticationForm()
+    
+    context = {
+        "form": form
+    }
+    
+    return render(request=request, template_name="first/login.html", context=context)
+
+
+def profile_view(request):
+    
+    if not request.user.is_authenticated:
+        return redirect("login_view")
+    
+    context = {
+        "user_details": request.user
+    }
+    
+    return render(request=request, template_name="first/profile.html", context=context)
+
+
+def logout_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login_view")
+    
+    logout(request=request)
+    
+    return redirect("login_view")
